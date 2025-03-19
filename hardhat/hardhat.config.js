ab634f02e5f250b9a973f6171cd195f01f6455c5
Martin Totovski <martin.totovski@endurosat.com>
2025-03-19 14:40:49 +0200

    Implemented environment variables

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,10 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.28",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
-    }
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+      chainId: 1337,
+      gasPrice: 8000000000
+    },
+  },
+  solidity: {
+    version: "0.8.28",
   }
 };
