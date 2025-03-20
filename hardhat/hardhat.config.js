e9b41b452313ec0d100c49b6147e3d8cfa29829e
Martin Totovski <martin.totovski@endurosat.com>
2025-03-20 14:56:30 +0200

    Added some tests

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,5 +3,10 @@ require("@nomicfoundation/hardhat-ignition-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+  },
   solidity: "0.8.28",
 };
